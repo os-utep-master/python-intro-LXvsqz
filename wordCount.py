f634a904500201d499d356720a8807744560c40f
Alex <avasquez31@miners.utep.edu>
2019-09-01 16:01:10 -0600

    was able to count the number of words, print on the same line. Missing some words need to figure out why, also need to work on writing to a new file

--- wordCount.py
+++ wordCount.py
@@ -3,15 +3,44 @@ import re
 val = input("Enter The File Name: ")
 count = 0
 
+
 readFile = open(val, "r")  # read mode
 
+wordDict = dict()
+
+
+def populate(name, occurrence):
+    wordDict.update({name: occurrence})
+
+
+def isPresent(name):
+    if name in wordDict.keys():
+        return True
+
+
 for word in readFile.read().split():  # for each word in the file, split
-    word = re.sub(r'[^\w]', '', word)  # using function of regular expression library
-    count += 1
-    print(word.lower())
-    print(count)
+    word = re.sub(r'[^\w\s]', '', word)  # using function of regular expression library
+
+    if isPresent(word):
+         #print(wordDict.get(word))
+        repeated = wordDict.get(word)
+        repeated += 1
+        wordDict[word] = repeated
+
+    else:
+        populate(word, 1)
+
+
+   # print(word.lower())
+
+    # print(count)
+for keys, values in sorted(wordDict.items()):
+    print(keys +":",values)
+
+
 
 newFile = ("newFile.txt", "w+")
+
 
 
 
