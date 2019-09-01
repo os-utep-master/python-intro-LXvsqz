c018eac89c3c515201d2730e00b4fd60c7e4e716
Alex <avasquez31@miners.utep.edu>
2019-09-01 12:54:59 -0600

    Removing all punctuation and replacing with empty string. Used regular expression library

--- wordCount.py
+++ wordCount.py
@@ -1,12 +1,15 @@
+import re
 
 val = input("Enter The File Name: ")
+count = 0
 
-#print(val)
+readFile = open(val, "r")  # read mode
 
-readFile = open(val, "r")  # mode r is for reading
-for word in readFile.read().split():
-
+for word in readFile.read().split():  # for each word in the file, split
+    word = re.sub(r'[^\w]', '', word)  # using function of regular expression library
+    count += 1
     print(word.lower())
+    print(count)
 
 newFile = ("newFile.txt", "w+")
 
